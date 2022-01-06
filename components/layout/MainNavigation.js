da8aa8e6515bddfd473ec81b75e1c134d63d9e0f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-06 15:59:26 -0600

    Fix Link component

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,22 +1,23 @@
-import classes from './MainNavigation.module.css';
+import Link from "next/link";
+
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-
-  return (
-    <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>All Meetups</Link>
-          </li>
-          <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>React Meetups</div>
+			<nav>
+				<ul>
+					<li>
+						<Link href="/">All Meetups</Link>
+					</li>
+					<li>
+						<Link href="/new-meetup">Add New Meetup</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default MainNavigation;
