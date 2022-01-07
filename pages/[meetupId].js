05e0c2bd6aa9e3e21439f094bb4cc94695b80abd
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-06 23:26:39 -0600

    Fix sample image links

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -5,7 +5,7 @@ import MeetupDetail from "../components/meetups/MeetupDetail";
 function MeetupDetails() {
 	return (
 		<MeetupDetail
-			image="https://unsplash.com/photos/8IKf54pc3qk"
+			image="https://images.unsplash.com/photo-1559925393-8be0ec4767c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
 			title="A First Meetup"
 			address="12 Random Street, Strange city 54312"
 			description="This is the first meetup"
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ const SAMPLE_MEETUPS = [
 	{
 		id: "m1",
 		title: "A First Meetup",
-		image: "https://unsplash.com/photos/8IKf54pc3qk",
+		image: "https://images.unsplash.com/photo-1559925393-8be0ec4767c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
 		address: "12 Random Street, Strange city 54312",
 		description: "This is the first meetup",
 	},
 	{
 		id: "m2",
 		title: "A Second Meetup",
-		image: "https://unsplash.com/photos/Ciqxn7FE4vE",
+		image: "https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
 		address: "65 Another Street, New city 42531",
 		description: "This is the second meetup",
 	},
