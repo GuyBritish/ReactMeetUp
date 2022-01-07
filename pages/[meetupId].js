e3b45bc2f20c0a8f7ffaa63878aa90446305b19f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-06 23:23:36 -0600

    Add sample details page

---
+++ pages/[meetupId].js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://unsplash.com/photos/8IKf54pc3qk"
+			title="A First Meetup"
+			address="12 Random Street, Strange city 54312"
+			description="This is the first meetup"
+		/>
+	);
+}
+
+export default MeetupDetails;
