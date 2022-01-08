7e03829ef40187207b52bdee43964aa2d246902f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-07 23:39:59 -0600

    Add request to internal api

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -2,12 +2,14 @@ import { useRouter } from "next/router";
 
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
+const axios = require("axios");
+
 function NewMeetupPage() {
 	const router = useRouter();
 
 	const addMeetupHandler = async (meetupData) => {
 		const options = {
-			url: "api/new-meetup",
+			url: "/api/new-meetup",
 			method: "POST",
 			data: meetupData,
 		};
