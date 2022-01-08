f502383bc106bc18de4daee7d9d511cca7a53192
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-07 22:50:44 -0600

    Add API request for new meetup

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,8 +1,19 @@
+import { useRouter } from "next/router";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-	const addMeetupHandler = (meetupData) => {
-		console.log(meetupData);
+	const router = useRouter();
+
+	const addMeetupHandler = async (meetupData) => {
+		const options = {
+			url: "api/new-meetup",
+			method: "POST",
+			data: meetupData,
+		};
+		const resp = await axios(options);
+
+		router.push("/");
 	};
 
 	return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
